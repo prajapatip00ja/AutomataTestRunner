f2183308620fd849dcbaa5cd1e4d9991a0ff6546
Pooja Prajapati <poojap@thoughtworks.com>
2016-06-06 00:19:34 +0530

    nfa generator complete without abslon

--- nfaGenerator.js
+++ nfaGenerator.js
@@ -4,35 +4,38 @@ var alphabets = ["0","1"];
 var initialState = "q1";
 var finalState = ["q2"];
 
-var transtion = {"q1" : {"0" : "q1","1": ["q1","q2"]},"q2":{"0":"null","1":"null"}}
+//var transtion = {"q1" : {"0" : "q1","1": ["q1","q2"]},"q2":{"0":"","1":""}}
+var transtion = {
+  "q1":{"0":["q1","q3"],"1":["q1","q2"]},"q2":{"0":"","1":"q4"},"q3":{"0":"q4","1":""},"q4":{"0":"q4","1":"q4"}
+}
 
-var input = "1000000001"
+var input = "1010101"
 
 var nfaGenerator = function(state,alphabets,initialState,finalState,transtion){
 
   var nfa =  function(val) {
-    var fs = val.reduce(function(pre,curr,ci,ar){
-      if(Array.isArray(pre)){
-        var finalStates = handleArrays(pre,ci,ar);
-        return finalStates
-      }
-      else{
-          var res =  transtion[pre] ? transtion[pre][curr] : "null"
-          return res
-        }
+    var fs = val.reduce(function(pre,curr){
+      if(Array.isArray(pre))
+        return _.flattenDeep(states(pre,curr));
+      return getState(pre,curr)
     },initialState)
     return fs;
   }
 
-  var handleArrays = function(pre,ci,ar){
-    return _.flattenDeep(pre.map(function(ele){
-      initialState = ele;
-      return nfa(ar.slice(ci))
-  }));
-}
+  var getState = function(pre,curr){
+    return transtion[pre] ? transtion[pre][curr] : "";
+  }
+
+  var states = function(pre,curr){
+    return pre.map(function(ele){
+      return getState(ele,curr)
+    })
+  }
 
   return nfa;
 }
+
+
 
 
 console.log("----------",nfaGenerator(state,alphabets,initialState,finalState,transtion)(input.split("")))
